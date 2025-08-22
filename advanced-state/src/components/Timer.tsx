c2b54f8e94691f56046f33e6dae4744c96d3fe6e
Aleksandra Sobańska <a.sobanska@alekseon.com>
2025-08-22 09:36:10 +0200

    finish project

--- advanced-state/src/components/Timer.tsx
+++ advanced-state/src/components/Timer.tsx
@@ -1,11 +1,33 @@
 import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { type Timer as TimerProps, useTimersContext } from '../store/timers-context.tsx';
+import { useEffect, useState } from 'react';
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const { isRunning } = useTimersContext();
+
+  useEffect(() => {
+    setRemainingTime(duration * 1000);
+  }, [duration]);
+
+  useEffect(() => {
+    if (!isRunning || remainingTime <= 0) return;
+
+    const id = window.setInterval(() => {
+      setRemainingTime(prev => Math.max(prev - 50, 0));
+    }, 50);
+
+    return () => clearInterval(id);
+  }, [isRunning, remainingTime]);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
